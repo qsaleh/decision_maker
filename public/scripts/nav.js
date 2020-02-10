d47dedbf697e94b8f8898b8b61e32839da336236
qsaleh <qmsaleh@gmail.com>
2020-02-10 16:04:09 +0000

    completed nav bar with log-in functionality

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -6,3 +6,29 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+jQuery(function($) {
+
+  $(document).ready(function(){
+    $('#user-login').on('submit', function (event){
+      event.preventDefault();
+      const data = $(this).serialize();
+      $.ajax({
+        url: '/login',
+        method: 'POST',
+        data,
+        success: (data) => {
+
+            alert('success!');
+        },
+        error: (error) => {
+            console.log('this request failed and this was the error', error);
+        }
+      });
+    })
+
+
+
+  })
+
+});
