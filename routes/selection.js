610f316c94578f520ffc82b6972ab39b37b4c06d
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-02-12 22:43:32 +0000

    ready to push

--- routes/selection.js
+++ routes/selection.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const router  = express.Router();
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const {scoringOptions}  = require('../public/scripts/scoringOptions.js');
 
 
@@ -41,61 +40,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
         `)
 
         .then (res => {
-=======
-const { scoringOptions }  = require('../public/scripts/scoringOptions.js');
-const sendEmailToUser = require('./sendgrid');
-module.exports = (db) => {
-
-  app.set("view engine", "ejs");
-  app.use(bodyParser.urlencoded({ extended: true }));
-  router.post("/selection", (req, res) => {
-    const setData = [];
-
-    for (const key in req.body) {
-
-      for (const key2 in req.body[key]) {
-      //  console.log(key2);
-        for (const key3 in req.body[key][key2]) {
-          setData.push(req.body[key][key2]['option']);
-
-        }
-      }
-
-    }
-
-    const data = Array.from(new Set(setData));
-
-
-    //ADDING IT TO THE DATABASE!
-
-    const objData = {};
-    data.forEach((data, index) => {
-
-      objData[`${data}`] = `${index + 1}`;
-      db.query(`UPDATE options
-      SET rank = ${index + 1}
-      WHERE option = '${data}';
-      `)
-
-        .then(res => {
->>>>>>> master
           console.log('success');
 
         })
         .catch(err => {
           console.log(err);
-<<<<<<< HEAD
-        })
-      })
-
-      const scores = scoringOptions(objData);
-
-
-    });
-
-  return router;
-};
-=======
         });
     });
 
@@ -105,4 +54,3 @@ module.exports = (db) => {
 
   return router;
 };
->>>>>>> master
