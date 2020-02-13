8fb7e4dd4e846476a390fc7fe9fe213128b4cfa5
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-02-13 01:07:39 +0000

    Fixed the bug that prevented db to display poll options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,17 +9,8 @@ $(() => {
       $('.poll').prepend(`<div id='pollTitle'><b>${results[0]["question"]}</b></div>
       <ul id='allOptions'>`);
       resultsArray = results;
-<<<<<<< HEAD
       for(const choice of results) {
         $('#allOptions').append(
-=======
-      for (const choice of results) {
-<<<<<<< HEAD
-        $('#allOptions').append(
-=======
-        $('#alloptions').append(
->>>>>>> 9829b204496967de29424bdd3169e1b8f71d9af3
->>>>>>> master
           `
             <li class='option' data-name= '${choice['option']}' id='option_${choice['option_id']}'>${choice['option']}</li>
           `
@@ -28,33 +19,22 @@ $(() => {
     }
   })
     .then(() => {
-<<<<<<< HEAD
 
 
-=======
->>>>>>> master
       $('#allOptions').sortable({
         start: function(event, ui) {
           $(this).attr('dataPreviousIndex', ui.item.index());
         },
         update: function(event, ui) {
-<<<<<<< HEAD
          const newIndex = ui.item.index();
           const oldIndex = $(this).attr('dataPreviousIndex');
           console.log('old', oldIndex);
           console.log('new', newIndex);
 
-=======
-          const newIndex = ui.item.index();
-          const oldIndex = $(this).attr('dataPreviousIndex');
-          console.log('old', oldIndex);
-          console.log('new', newIndex);
->>>>>>> master
           let swapped = resultsArray[oldIndex];
           resultsArray[oldIndex] = resultsArray[newIndex];
           resultsArray[newIndex] = swapped;
           console.log('oldArray', resultsArray[oldIndex], 'newArray', resultsArray[newIndex], 'Array', resultsArray);
-<<<<<<< HEAD
 
 
         }
@@ -87,27 +67,5 @@ $('.button').click(function() {
   });
 
 
-=======
-        }
-      });
-    });
-  $('.button').click(function() {
-    // event.preventDefault();
-    $('nav').slideDown();
-    $('header').slideDown();
-    $('.pollContainer').hide();
-    $('.afterSubmission').show();
-    //POST REQUEST
-    $.post("/api/selection/selection",
-      {
-        data: resultsArray
-      }
-    );
-    setTimeout(() => {
-      window.location = 'http://localhost:8080/';
-  
-    }, 2000);
-  });
->>>>>>> master
 });
 
