b707e1e7443c66a00a1c91e086ba1684b35b88b8
qsaleh <qmsaleh@gmail.com>
2020-02-14 18:48:26 +0000

    final commit

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -10,8 +10,8 @@ const sendEmailToUser = function(email, pollId) {
     to: email,
     from: 'GetYoVerdict@verdict.com',
     subject: 'Check out Yo Verdict',
-    html: `Hey friend! <br/>Thank you for using Verdict! <br/>Found below is the link for your results and to send your friends the poll!<br/> Cheers!<br/> Get Yo Verdict team<br/>View your results here:   <a  href="https://fathomless-dusk-70706.herokuapp.com/results/${pollId}"></a>
-          <br/>Send this link to your friends:<a  href="https://fathomless-dusk-70706.herokuapp.com/selection/${pollId}"></a>`,
+    html: `Hey friend! <br/>Thank you for using Verdict! <br/>Found below is the link for your results and to send your friends the poll!<br/> Cheers!<br/> Get Yo Verdict team<br/>View your results here:   <a  href="https://fathomless-dusk-70706.herokuapp.com/results/${pollId}">www.getyoverdict.com/results</a>
+          <br/>Send this link to your friends:<a  href="https://fathomless-dusk-70706.herokuapp.com/selection/${pollId}">www.getyoverdict.com/select-your-choices</a>`,
   };
   sgMail
     .send(msg)
